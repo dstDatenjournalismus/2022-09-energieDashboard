37398dcae6fec3a6f47875225a21f21dcf66de5d
RobinKohrs <robin.kohrs@gmx.de>
2022-09-26 14:38:45 +0200

    why is download.file getting a 503?

--- R/naturalGasRussiaToEurope.R
+++ R/naturalGasRussiaToEurope.R
@@ -11,9 +11,7 @@ url = "https://www.bruegel.org/sites/default/files/2022-09/gas_tracker_update_.z
 download_path = tempfile()
 download_dir = dirname(download_path)
 
-
-
-download.file(url, download_path, headers = c("User-Agent" = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"))
+download.file(url, download_path, method="libcurl", headers = c("User-Agent" = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"))
 
 # unzip data --------------------------------------------------------------
 unzip(download_path, exdir = download_dir)
