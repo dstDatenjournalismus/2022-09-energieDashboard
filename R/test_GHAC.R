9ee9d1bd2770c27c09d70de3666b2217df49a334
RobinKohrs <robin.kohrs@gmx.de>
2022-09-26 15:04:16 +0200

    using the test script...

--- R/test_GHAC.R
+++ R/test_GHAC.R
@@ -16,12 +16,24 @@ print(files)
 
 
 # find file
-files = dir(download_dir, "*.csv|xlsx")
+files = dir(download_dir, "*.csv|xlsx", full.names = T)
 file_idx = which(grepl("country_data", files))
 
 # read data
 data = read.csv(files[[file_idx]])
 
+# wrangle it a bit
+col_indices = which(grepl("week|Russia_", names(data)))
+data = data[, col_indices]
+data = data[complete.cases(data), ]
+
+data[["EU Ziel"]] = (1/3) * data$Russia_2021
+
 print(head(data))
 
+names(data)[names(data) == "Russia_avg"] = "Durchschnitt 2015 - 2022"
+names(data)[names(data) == "Russia_2022"] = "2022"
 
+output_file = "R/output/natural_gas_russia_europe.csv"
+dir = dirname(output_file); if(!dir.exists(dir)) dir.create(dir, recursive = T)
+write.csv(data, output_file)
