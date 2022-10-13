dfa53ac19d11eac5d2ce80b59ea65126dc674942
RobinKohrs <robin.kohrs@gmx.de>
2022-10-13 15:22:50 +0200

    thanks for changing the entire structure

--- R/get_natural_gas_russia_europe.R
+++ R/get_natural_gas_russia_europe.R
@@ -1,7 +1,8 @@
 library(httr)
 
 # url ---------------------------------------------------------------------
-url = "https://www.bruegel.org/sites/default/files/2022-09/gas_tracker_update_.zip"
+# url = "https://www.bruegel.org/sites/default/files/2022-09/gas_tracker_update_.zip"
+url = "https://www.bruegel.org/sites/default/files/2022-10/gas%20datasets_0.zip"
 
 
 # download zip ------------------------------------------------------------
@@ -11,8 +12,12 @@ download_dir = dirname(download_path)
 # download.file(url, download_path, method="libcurl", headers = c("User-Agent" = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"))
 httr::GET(url, write_disk(download_path, overwrite = T))
 unzip(download_path, exdir = download_dir)
-files = dir(download_dir)
-print("files: ", files)
+
+
+# data dir ----------------------------------------------------------------
+unzipped_dir_name = paste0(download_dir, "/gas datasets")
+
+files = dir(unzipped_dir_name)
 
 # find file
 files = dir(download_dir, "*.csv|xlsx", full.names = T)
