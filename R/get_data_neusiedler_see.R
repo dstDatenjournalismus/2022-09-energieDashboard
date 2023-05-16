c37677d3861e8dbf18f104585200bef126aa984a
RobinKohrs <robin.kohrs@gmx.de>
2023-05-16 11:36:08 +0200

    no need of tidyverse

--- R/get_data_neusiedler_see.R
+++ R/get_data_neusiedler_see.R
@@ -1,7 +1,4 @@
 library(jsonlite)
-library(tidyverse)
-library(rajudas)
-
 
 data_raw = fromJSON("https://wasser.bgld.gv.at/?type=200365&tx_lshydrography_annualdata[zrid]=hjpRalsA7imnA-XjIsk6qQ", simplifyVector = F, simplifyDataFrame = F)
 df = data.frame(do.call("rbind", data_raw))
