a64723feda5c15d7b5b7df9c3b1c32e6a8d3c3b5
RobinKohrs <robin.kohrs@gmx.de>
2023-05-16 11:43:10 +0200

    unlist each column

--- R/get_data_neusiedler_see.R
+++ R/get_data_neusiedler_see.R
@@ -2,5 +2,6 @@ library(jsonlite)
 
 data_raw = fromJSON("https://wasser.bgld.gv.at/?type=200365&tx_lshydrography_annualdata[zrid]=hjpRalsA7imnA-XjIsk6qQ", simplifyVector = F, simplifyDataFrame = F)
 df = data.frame(do.call("rbind", data_raw))
+df_unlist = as.data.frame(apply(df, 2, FUN = unlist))
 op_neusiedler_see = "R/output/wasserstand_neusiedlerSee.csv"
-write.csv(df, op_neusiedler_see)
+write.csv(df_unlist, op_neusiedler_see)
